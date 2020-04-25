8c8bcc2f1311b052826b4439aff0a3b927142be3
ddqof <ddqof@users.noreply.github.com>
2020-04-25 22:01:55 +0500

    Add function for checking obstacles like walls and eat

--- snake_components.py
+++ snake_components.py
@@ -5,6 +5,7 @@ from config import WIDTH, HEIGHT, BLOCK_SIZE
 
 class Vector:
     """Компонент «вектор змейки»"""
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -12,6 +13,7 @@ class Vector:
 
 class Block:
     """Компонент «блок змейки»"""
+
     def __init__(self, x, y, canvas):
         self.image = canvas.create_rectangle(x, y,
                                              x + BLOCK_SIZE, y + BLOCK_SIZE,
@@ -20,6 +22,7 @@ class Block:
 
 class Snake:
     """Компонент «змейка»"""
+
     def __init__(self, blocks, canvas):
         self.blocks = blocks
         self.canvas = canvas
@@ -27,19 +30,30 @@ class Snake:
         self.IN_GAME = True
 
     def move(self):
+        print(self.canvas.coords(self.canvas.eat))
         for index in reversed(range(1, len(self.blocks))):
             block = self.blocks[index].image
             x1, y1, x2, y2 = self.canvas.coords(self.blocks[index - 1].image)
             self.canvas.coords(block, x1, y1, x2, y2)
 
         x1, y1, x2, y2 = self.canvas.coords(self.blocks[0].image)
-
         self.canvas.coords(self.blocks[0].image,
                            x1 + self.vector.x * BLOCK_SIZE,
                            y1 + self.vector.y * BLOCK_SIZE,
                            x2 + self.vector.x * BLOCK_SIZE,
                            y2 + self.vector.y * BLOCK_SIZE)
+        self.check_obstacles()
 
+    def check_obstacles(self):
+        self.check_eat()
+        self.check_walls()
+
+    def check_eat(self):
+        if len(self.canvas.coords(self.canvas.eat)) != 0:
+            if self.canvas.coords(self.blocks[0].image) == self.canvas.coords(self.canvas.eat):
+                self.canvas.delete(self.canvas.eat)
+
+    def check_walls(self):
         if (self.canvas.coords(self.blocks[0].image)[2] > WIDTH or
                 self.canvas.coords(self.blocks[0].image)[0] < 0 or
                 self.canvas.coords(self.blocks[0].image)[3] > HEIGHT or
--- snake_engine.py
+++ snake_engine.py
@@ -11,7 +11,7 @@ class Master(tk.Canvas):
     def __init__(self, root, **kwargs):
         super(Master, self).__init__(root, kwargs)
         self.root = root
-        self.create_eat()
+        self.eat = self.create_eat()
         self.blocks = [snake_components.Block(BLOCK_SIZE * 3, BLOCK_SIZE, self),
                        snake_components.Block(BLOCK_SIZE * 2, BLOCK_SIZE, self),
                        snake_components.Block(BLOCK_SIZE, BLOCK_SIZE, self)]
@@ -24,14 +24,17 @@ class Master(tk.Canvas):
             self.root.after(int(1 / SNAKE_SPEED * 1000), self.play_game)
 
     def create_eat(self):
-        x = BLOCK_SIZE * random.randint(1, BLOCK_SIZE * ((WIDTH / BLOCK_SIZE ** 2) - 1))
-        y = BLOCK_SIZE * random.randint(1, (HEIGHT * BLOCK_SIZE) / (BLOCK_SIZE ** 2) - BLOCK_SIZE)
-        self.create_oval(x, y, x + BLOCK_SIZE, y + BLOCK_SIZE,
-                         fill="red")
+        x1 = BLOCK_SIZE * random.randint(1, BLOCK_SIZE * ((WIDTH / BLOCK_SIZE ** 2) - 1))
+        y1 = BLOCK_SIZE * random.randint(1, (HEIGHT * BLOCK_SIZE) / (BLOCK_SIZE ** 2) - BLOCK_SIZE)
+        x2 = x1 + BLOCK_SIZE
+        y2 = y1 + BLOCK_SIZE
+        eat = self.create_oval(x1, y1, x2, y2, fill="red")
+        return eat
 
 
 def main():
     """Запуск игрового процесса"""
+
     parse_args()
     root = tk.Tk()
     root.title("Snake")
@@ -45,8 +48,9 @@ def main():
 
 def parse_args():
     """Разбор аргуметов запуска"""
+
     parser = argparse.ArgumentParser(
-        prog='snake.py',
+        prog='snake_engine.py',
         description='''Snake game. Use W-A-S-D or Arrow keys to 
         control the snake.''',
         epilog='''Author: Dmitry Podaruev <ddqof.vvv@gmail.com>'''
