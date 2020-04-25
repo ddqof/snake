369dc0674537f5946cf5ce55b0dca4feb51ee96e
ddqof <ddqof@users.noreply.github.com>
2020-04-25 19:38:19 +0500

    Reform
    Add Master class

--- snake.py
+++ snake.py
@@ -1,103 +1,57 @@
 #!/usr/bin/env python3
 
 import tkinter as tk
-
-WIDTH = 800
-HEIGHT = 600
-BLOCK_SIZE = 20
-SNAKE_SPEED = 20
-
-
-class Vector:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+import snake_components
+import argparse
+import random
+from config import SNAKE_SPEED, BLOCK_SIZE, WIDTH, HEIGHT
 
 
-class Block:
-    def __init__(self, x, y, canvas):
-        self.image = canvas.create_rectangle(x, y,
-                                             x + BLOCK_SIZE, y + BLOCK_SIZE,
-                                             fill='white')
-
-
-class Snake:
-    def __init__(self, blocks, canvas):
-        self.blocks = blocks
-        self.canvas = canvas
-        self.vector = Vector(1, 0)
-        self.IN_GAME = True
-
-    def move(self):
-        for index in reversed(range(1, len(self.blocks))):
-            block = self.blocks[index].image
-            x1, y1, x2, y2 = self.canvas.coords(self.blocks[index - 1].image)
-            self.canvas.coords(block, x1, y1, x2, y2)
-
-        x1, y1, x2, y2 = self.canvas.coords(self.blocks[1].image)
-
-        self.canvas.coords(self.blocks[0].image,
-                           x1 + self.vector.x * BLOCK_SIZE,
-                           y1 + self.vector.y * BLOCK_SIZE,
-                           x2 + self.vector.x * BLOCK_SIZE,
-                           y2 + self.vector.y * BLOCK_SIZE)
-
-        if (self.canvas.coords(self.blocks[0].image)[2] > WIDTH or
-                self.canvas.coords(self.blocks[0].image)[0] < 0 or
-                self.canvas.coords(self.blocks[0].image)[3] > HEIGHT or
-                self.canvas.coords(self.blocks[0].image)[1] < 0):
-            self.IN_GAME = False
-            self.canvas.create_text(WIDTH / 2, HEIGHT / 2, text="Game Over",
-                                    justify=tk.CENTER, font="Verdana 18",
-                                    fill="white")
-
-    def key_handle(self, event):
-        if event.keycode == 83 or event.keycode == 40:
-            if self.vector.y != -1:
-                self.vector = Vector(0, 1)
-
-        if event.keycode == 87 or event.keycode == 38:
-            if self.vector.y != 1:
-                self.vector = Vector(0, -1)
-
-        if event.keycode == 68 or event.keycode == 39:
-            if self.vector.x != -1:
-                self.vector = Vector(1, 0)
-
-        if event.keycode == 65 or event.keycode == 37:
-            if self.vector.x != 1:
-                self.vector = Vector(-1, 0)
-
-
-class Engine:
-    def __init__(self, root, snake):
+class Master(tk.Canvas):
+    def __init__(self, root, **kwargs):
+        super(Master, self).__init__(root, kwargs)
         self.root = root
-        self.snake = snake
+        self.create_eat()
+        self.blocks = [snake_components.Block(BLOCK_SIZE * 3, BLOCK_SIZE, self),
+                       snake_components.Block(BLOCK_SIZE * 2, BLOCK_SIZE, self),
+                       snake_components.Block(BLOCK_SIZE, BLOCK_SIZE, self)]
+        self.snake = snake_components.Snake(self.blocks, self)
+        self.bind("<KeyPress>", self.snake.key_handle)
 
-    def start_game(self):
+    def play_game(self):
         self.snake.move()
         if self.snake.IN_GAME:
-            self.root.after(int(1/SNAKE_SPEED *  1000), self.start_game)
+            self.root.after(int(1 / SNAKE_SPEED * 1000), self.play_game)
+
+    def create_eat(self):
+        x = BLOCK_SIZE * random.randint(1, BLOCK_SIZE * ((WIDTH / BLOCK_SIZE ** 2) - 1))
+        y = BLOCK_SIZE * random.randint(1, (HEIGHT * BLOCK_SIZE) / (BLOCK_SIZE ** 2) - BLOCK_SIZE)
+        self.create_oval(x, y, x + BLOCK_SIZE, y + BLOCK_SIZE,
+                         fill="red")
 
 
 def main():
+    """Запуск игрового процесса"""
+    parse_args()
     root = tk.Tk()
     root.title("Snake")
-    c = tk.Canvas(root, width=WIDTH, height=HEIGHT, bg="#3F888F")
-    c.pack()
-    c.focus_set()
-    # tk.Label(text="Score: {0}\n"
-    #                    "High Score: {1}".format(SCORE, HIGH_SCORE),
-    #               width=20, height=3).pack()
-    blocks = [Block(BLOCK_SIZE * 3, BLOCK_SIZE, c),
-              Block(BLOCK_SIZE * 2, BLOCK_SIZE, c),
-              Block(BLOCK_SIZE, BLOCK_SIZE, c)]
-    snake = Snake(blocks, c)
-    c.bind("<KeyPress>", snake.key_handle)
-    game_engine = Engine(root, snake)
-    game_engine.start_game()
+    game_engine = Master(root, width=WIDTH, height=HEIGHT, bg="black")
+    game_engine.pack()
+    game_engine.focus_set()
+    game_engine.play_game()
 
     root.mainloop()
+
+
+def parse_args():
+    """Разбор аргуметов запуска"""
+    parser = argparse.ArgumentParser(
+        prog='snake.py',
+        description='''Snake game. Use W-A-S-D or Arrow keys to 
+        control the snake.''',
+        epilog='''Author: Dmitry Podaruev <ddqof.vvv@gmail.com>'''
+    )
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
---
+++ snake_components.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+import tkinter as tk
+from config import WIDTH, HEIGHT, BLOCK_SIZE
+
+
+class Vector:
+    """Компонент «вектор змейки»"""
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Block:
+    """Компонент «блок змейки»"""
+    def __init__(self, x, y, canvas):
+        self.image = canvas.create_rectangle(x, y,
+                                             x + BLOCK_SIZE, y + BLOCK_SIZE,
+                                             fill='white')
+
+
+class Snake:
+    """Компонент «змейка»"""
+    def __init__(self, blocks, canvas):
+        self.blocks = blocks
+        self.canvas = canvas
+        self.vector = Vector(1, 0)
+        self.IN_GAME = True
+
+    def move(self):
+        for index in reversed(range(1, len(self.blocks))):
+            block = self.blocks[index].image
+            x1, y1, x2, y2 = self.canvas.coords(self.blocks[index - 1].image)
+            self.canvas.coords(block, x1, y1, x2, y2)
+
+        x1, y1, x2, y2 = self.canvas.coords(self.blocks[0].image)
+
+        self.canvas.coords(self.blocks[0].image,
+                           x1 + self.vector.x * BLOCK_SIZE,
+                           y1 + self.vector.y * BLOCK_SIZE,
+                           x2 + self.vector.x * BLOCK_SIZE,
+                           y2 + self.vector.y * BLOCK_SIZE)
+
+        if (self.canvas.coords(self.blocks[0].image)[2] > WIDTH or
+                self.canvas.coords(self.blocks[0].image)[0] < 0 or
+                self.canvas.coords(self.blocks[0].image)[3] > HEIGHT or
+                self.canvas.coords(self.blocks[0].image)[1] < 0):
+            self.IN_GAME = False
+            self.canvas.create_text(WIDTH / 2, HEIGHT / 2, text="Game Over",
+                                    justify=tk.CENTER, font="Verdana 18",
+                                    fill="white")
+
+    def key_handle(self, event):
+        if event.keycode == 83 or event.keycode == 40:
+            if self.vector.y == 0 and self.IN_GAME:
+                self.vector = Vector(0, 1)
+                self.move()
+
+        if event.keycode == 87 or event.keycode == 38:
+            if self.vector.y == 0 and self.IN_GAME:
+                self.vector = Vector(0, -1)
+                self.move()
+
+        if event.keycode == 68 or event.keycode == 39:
+            if self.vector.x == 0 and self.IN_GAME:
+                self.vector = Vector(1, 0)
+                self.move()
+
+        if event.keycode == 65 or event.keycode == 37:
+            if self.vector.x == 0 and self.IN_GAME:
+                self.vector = Vector(-1, 0)
+                self.move()
