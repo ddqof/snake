2e9eddd2b55aa238cef4f98946f42dcd8b4bb4a9
ddqof <ddqof@users.noreply.github.com>
2020-05-18 23:00:28 +0500

    add more tests

--- tests.py
+++ tests.py
@@ -5,9 +5,9 @@ import snake_engine
 import snake_components
 
 
-class TestSnakeInteractionsWithMap(unittest.TestCase):
+class SnakeMove(unittest.TestCase):
     def setUp(self):
-        self.engine = snake_engine.Driver(r'test_levels\interactions', None)
+        self.engine = snake_engine.Driver(r'test_levels\move', None)
 
     def move_and_get_snake_coords(self, count):
         for i in range(count):
@@ -73,39 +73,6 @@ class TestSnakeInteractionsWithMap(unittest.TestCase):
         self.engine.snake.move()
         self.assertEqual(expected, result, "Snake is moving but game is over")
 
-    def test_interact_with_default_food(self):
-        self.engine.snake.vector = snake_components.Vector(0, 1)
-        for i in range(3):
-            self.engine.snake.move()
-        self.engine.snake.vector = snake_components.Vector(1, 0)
-        self.engine.snake.move()
-        self.engine.snake.check_eat()
-        result = self.move_and_get_snake_coords(1)
-        expected = [(12, 16), (11, 16), (10, 16), (10, 15), (10, 14), (10, 13), (9, 13)]
-        self.assertEqual(len(expected), len(result), "Snake ate food but didn't grow up")
-        self.assertEqual(expected, result, "Snake grow up, but for something went wrong with her blocks position")
-
-    def test_interact_with_double_length_food(self):
-
-        self.engine.snake.vector = snake_components.Vector(1, 0)
-        for i in range(8):
-            self.engine.snake.move()
-        self.engine.snake.vector = snake_components.Vector(0, -1)
-        for i in range(2):
-            self.engine.snake.move()
-        print(self.engine.snake.blocks[0].map_coords)
-        for row in self.engine.map:
-            for x in row:
-                print("{:4d}".format(x), end="")
-            print()
-
-        print('\n')
-        self.engine.snake.check_eat()
-        result = self.move_and_get_snake_coords(7)
-
-        expected = [(18, 5), (18, 6), (18, 7), (18, 8), (18, 9), (18, 10), (18, 11), (18, 12), (18, 13), (17, 13), (16, 13), (15, 13)]
-        self.assertEqual(len(expected), len(result), "Snake ate food but didn't grow up 2 times")
-
     def test_self_eating(self):
         self.engine.snake.vector = snake_components.Vector(0, 1)
         self.engine.snake.move()
@@ -119,6 +86,84 @@ class TestSnakeInteractionsWithMap(unittest.TestCase):
         self.assertEqual(expected, result, "Snake moves wrong for not straight way")
         self.engine.snake.move()
         self.assertEqual(expected, result, "Snake is moving but game is over")
+
+
+class SnakeInteractionsWithMap(unittest.TestCase):
+
+    def move_and_get_snake_coords(self, count):
+        for i in range(count):
+            self.engine.snake.move()
+        coords = []
+        for block in self.engine.snake.blocks:
+            coords.append(block.map_coords)
+        return coords
+
+    def test_interact_with_default_food(self):
+        self.engine = snake_engine.Driver(r'test_levels\default_food', None)
+        self.engine.snake.vector = snake_components.Vector(1, 0)
+        self.engine.snake.move()
+        self.engine.snake.check_eat()
+        result = self.move_and_get_snake_coords(1)
+        expected = [(12, 13), (11, 13), (10, 13), (9, 13), (8, 13), (7, 13), (6, 13)]
+        self.assertEqual(len(expected), len(result), "Snake ate food but didn't grow up")
+        self.assertEqual(expected, result, "Snake grow up, but for something went wrong with her blocks position")
+
+    def test_interact_with_double_length_food(self):
+        self.engine = snake_engine.Driver(r'test_levels\double_length', None)
+        self.engine.snake.vector = snake_components.Vector(1, 0)
+        self.engine.snake.move()
+        self.engine.snake.check_eat()
+        result = self.move_and_get_snake_coords(7)
+        expected = [(18, 13), (17, 13), (16, 13), (15, 13), (14, 13), (13, 13), (12, 13), (11, 13), (10, 13), (9, 13),
+                    (8, 13), (7, 13)]
+        self.assertEqual(len(expected), len(result), "Snake ate food but didn't grow up 2 times")
+        self.assertEqual(expected, result, "Snake grow up, but for something went wrong with her blocks position")
+
+    def test_interact_with_reverse_food_to_left_side(self):
+        self.engine = snake_engine.Driver(r'test_levels\reverse_to_left', None)
+        self.engine.snake.vector = snake_components.Vector(1, 0)
+        self.engine.snake.move()
+        self.engine.snake.check_eat()
+        result = self.move_and_get_snake_coords(1)
+        expected = [(5, 13), (6, 13), (7, 13), (8, 13), (9, 13), (10, 13)]
+        self.assertEqual(snake_components.Vector(-1, 0), self.engine.snake.vector)
+        self.assertEqual(expected, result)
+
+    def test_interact_with_reverse_food_to_right_side(self):
+        self.engine = snake_engine.Driver(r'test_levels\reverse_to_right', None)
+        self.engine.snake.vector = snake_components.Vector(0, 1)
+        self.engine.snake.move()
+        self.engine.snake.vector = snake_components.Vector(-1, 0)
+        for i in range(6):
+            self.engine.snake.move()
+        self.engine.snake.check_eat()
+        result = self.move_and_get_snake_coords(2)
+        expected = [(11, 14), (10, 14), (9, 14), (8, 14), (7, 14), (6, 14)]
+        self.assertEqual(snake_components.Vector(1, 0), self.engine.snake.vector)
+        self.assertEqual(expected, result)
+
+    def test_interact_with_reverse_food_up(self):
+        self.engine = snake_engine.Driver(r'test_levels\reverse_up', None)
+        self.engine.snake.vector = snake_components.Vector(0, 1)
+        for i in range(7):
+            self.engine.snake.move()
+        self.engine.snake.check_eat()
+        result = self.move_and_get_snake_coords(2)
+        expected = [(10, 13), (10, 14), (10, 15), (10, 16), (10, 17), (10, 18)]
+        self.assertEqual(snake_components.Vector(0, -1), self.engine.snake.vector)
+        self.assertEqual(expected, result)
+
+    def test_interact_with_reverse_food_down(self):
+        self.engine = snake_engine.Driver(r'test_levels\reverse_down', None)
+        self.engine.snake.vector = snake_components.Vector(0, -1)
+        for i in range(7):
+            self.engine.snake.move()
+        self.engine.snake.check_eat()
+        print(self.engine.snake.vector.x, self.engine.snake.vector.y)
+        result = self.move_and_get_snake_coords(2)
+        expected = [(10, 13), (10, 12), (10, 11), (10, 10), (10, 9), (10, 8)]
+        self.assertEqual(snake_components.Vector(0, 1), self.engine.snake.vector)
+        self.assertEqual(expected, result)
 
 
 if __name__ == '__main__':
